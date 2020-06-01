eac584d4f522117735e159beb10fd672b4331ea6
dace <dhruvindsd@gmail.com>
2020-06-01 18:22:07 +0530

    Updates

---
+++ precache-manifest.095fe696eeeb544d1ba371bd789eb879.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1192127aee17fd61d6bc94181ccab72c",
+    "url": "/support_genie/index.html"
+  },
+  {
+    "revision": "43b4a58d1361d21150c9",
+    "url": "/support_genie/static/js/2.2f531b93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/support_genie/static/js/2.2f531b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8de889e694ba884ea7",
+    "url": "/support_genie/static/js/main.ae898126.chunk.js"
+  },
+  {
+    "revision": "e62d459ccf58fa258755",
+    "url": "/support_genie/static/js/runtime-main.daa3adea.js"
+  }
+]);
